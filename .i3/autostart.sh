26b6a74d89653870ab8bc8aafa7c578347b746d5
Cedric Haase <cedric@sineband.de>
2016-08-27 00:55:16 +0200

    Remove unneeded code

--- .i3/autostart.sh
+++ .i3/autostart.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 # sourced by ~/.i3/config
 
-# Set a background color
-BG=""
-if which hsetroot >/dev/null 2>&1; then
-    BG=hsetroot
-else
-    if which esetroot >/dev/null 2>&1; then
-	BG=esetroot
-    else
-	if which xsetroot >/dev/null 2>&1; then
-	    BG=xsetroot
-	fi
-    fi
-fi
-test -z $BG || $BG -solid "#303030"
-
 # D-bus
 if which dbus-launch >/dev/null 2>&1 && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
        eval `dbus-launch --sh-syntax --exit-with-session`
